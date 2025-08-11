b9cd02104fe00b24b2dca7a84cfdf855e7017833
Brendon <you@example.combrendon_gama@hotmail.com>
2025-08-11 15:21:50 -0300

    Java - Complexidade de Algoritmos - 1. Complexidade O(n) – Busca Linear

---
+++ complexidade/1/PessoasArray.java
@@ -0,0 +1,40 @@
+public class PessoasArray {
+
+    private String[] nomes;
+
+    public PessoasArray() {
+        nomes =new String[]{"Amanda", "Beatriz", "Carlos","Daniela","Eduardo",
+                "Fabio","Gustavo", "Hingrid","Isabela","Joao","Leise","Maria",
+                "Norberto","Otavio","Paulo", "Quirino","Renata","Sabata",
+                "Tais","Umberto","Vanessa","Xavier"};
+    }
+
+    public String[] getNomes() {
+        return nomes;
+    }
+
+    public void setNomes(String[] nomes) {
+        this.nomes = nomes;
+    }
+
+    // implementar o método de buscaLinear
+    public void buscaLinear(String nome){
+    	boolean encontrado = false;
+    	int index = 0;
+    	System.out.println("Procurando pelo nome: "+nome);
+    	for (String nomePessoa : nomes) {
+    		System.out.println("Passando pelo indice: "+index);
+			if(nomePessoa.equals(nome)) {
+				encontrado = true;
+				System.out.println("Nome pesquisado é "+nome+" que está na posição "+index);
+			}
+			index++;			
+		}
+    	if(!encontrado) {
+    		throw new IllegalArgumentException("O nome "+nome+ " não se encontra no array de nomes");
+    	}
+        
+    	
+
+    }
+}

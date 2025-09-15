de7de4175449d9647c8cfea220970868b3caee3d
Brendon <you@example.combrendon_gama@hotmail.com>
2025-09-15 08:55:28 -0300

    1Java - Lambda Expressions - Exercícios de fixação - . Consultar Produtos com Predicado

---
+++ Lambda_Exercicios/predicado/ConsultaProdutos.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import Lambda_Exercicios.consulta_produtos.Produto;
+
+public class ConsultaProdutos {
+
+	public class ConsultaProdutos {
+		public static List<Produto> filtrar(List<Produto> produtos, Predicate<Produto> criterio) {
+			List<Produto> resultado = new ArrayList<>();
+			for (Produto produto : produtos) {
+	            if (criterio.test(produto)) {
+	                resultado.add(produto);
+	            }
+	        }
+	        return resultado;
+		}
+	}
+}

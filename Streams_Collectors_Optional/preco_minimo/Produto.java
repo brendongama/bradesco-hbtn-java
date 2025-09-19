7432af5f671ef7a1b42ab326b6cb66560644835b
Brendon <you@example.combrendon_gama@hotmail.com>
2025-09-19 08:53:25 -0300

    Java - Streams, Collectors e Optional - 2. Relatório Produtos por preco minimo

---
+++ Streams_Collectors_Optional/preco_minimo/Produto.java
@@ -0,0 +1,61 @@
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+public class Produto {
+
+	private int codigo;
+	private String nome;
+	private CategoriaProduto categoria ;
+	private double preco;
+	
+	@Override
+	public String toString() {
+		DecimalFormatSymbols symbols = new DecimalFormatSymbols(new Locale("pt", "BR"));
+	    symbols.setDecimalSeparator(',');
+	    symbols.setGroupingSeparator('\0');
+	    DecimalFormat formato = new DecimalFormat("0.00", symbols);
+	    return String.format("[%d] %s %s R$ %s", codigo, nome, categoria, formato.format(preco));
+	}
+
+	public Produto(int codigo, String nome, CategoriaProduto categoria, double preco) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.categoria = categoria;
+		this.preco = preco;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public CategoriaProduto getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(CategoriaProduto categoria) {
+		this.categoria = categoria;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+		
+}

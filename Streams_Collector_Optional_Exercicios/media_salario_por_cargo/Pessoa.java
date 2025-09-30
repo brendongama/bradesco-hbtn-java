9b1d9ba1fc575d06924efb373b35429f742e1e8e
Brendon <you@example.combrendon_gama@hotmail.com>
2025-09-30 10:17:29 -0300

    4. Hobbies por cargo

--- Streams_Collector_Optional_Exercicios/media_salario_por_cargo/Pessoa.java
+++ Streams_Collector_Optional_Exercicios/media_salario_por_cargo/Pessoa.java
@@ -1,55 +1,59 @@
+import java.util.List;
+
 public class Pessoa implements Comparable<Pessoa> {
 
-private int codigo;
-private String nome;
-private String cargo;
-private int idade;
-private double salario;
+	private int codigo;
+	private String nome;
+	private String cargo;
+	private int idade;
+	private double salario;
+	private List<String> hobbies;
 
-public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
-    this.codigo = codigo;
-    this.nome = nome;
-    this.cargo = cargo;
-    this.idade = idade;
-    this.salario = salario;
-}
+	
 
+	public Pessoa(int codigo, String nome, String cargo, int idade, double salario, List<String> hobbies) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.cargo = cargo;
+		this.idade = idade;
+		this.salario = salario;
+		this.hobbies = hobbies;
+	}
 
-public int getCodigo() {
-    return codigo;
-}
+	public int getCodigo() {
+		return codigo;
+	}
 
+	public String getNome() {
+		return nome;
+	}
 
-public String getNome() {
-    return nome;
-}
+	public String getCargo() {
+		return cargo;
+	}
 
+	public int getIdade() {
+		return idade;
+	}
 
-public String getCargo() {
-    return cargo;
-}
+	public double getSalario() {
+		return salario;
+	}
 
+	public List<String> getHobbies() {
+		return hobbies;
+	}
 
-public int getIdade() {
-    return idade;
-}
+	@Override
+	public String toString() {
+		return String.format(new java.util.Locale("pt", "BR"), "[%d] %s %s %d R$ %f", codigo, nome, cargo, idade,
+				salario);
+	}
 
-
-public double getSalario() {
-    return salario;
-}
-
-
-@Override
-public String toString() {
-    return String.format(new java.util.Locale("pt", "BR"),"[%d] %s %s %d R$ %f", codigo, nome, cargo, idade, salario);
-}
-
-
-@Override
-    public int compareTo(Pessoa outraPessoa) {
-        return this.nome.compareTo(outraPessoa.nome);
-    }
-
+	@Override
+	public int compareTo(Pessoa outraPessoa) {
+		return this.nome.compareTo(outraPessoa.nome);
+	}
 
 }
